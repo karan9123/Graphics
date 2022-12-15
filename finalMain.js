bb4584b9add82b04408f39f04aa389e8a2ded6ff
karan9123 <karan9123@gmail.com>
2022-12-15 02:39:27 -0500

    Update finalMain.js

--- finalMain.js
+++ finalMain.js
@@ -15,7 +15,11 @@ let globalProgram;
 // VAOs for the objects
 
 // textures
-let newTexture;
+let stemTexture;
+let tableTexture;
+let sphereTexture;
+let coneTexture;
+
 // rotation
 let curTexture = "globe";
 
@@ -74,7 +78,7 @@ function setUpCamera(program) {
   // set up your view
   // defaut is at (0,0,-5) looking at the origin
   let viewMatrix = glMatrix.mat4.create();
-  glMatrix.mat4.lookAt(viewMatrix, [0, 0, -9], [0, 0, 0], [0, 1, 0]);
+  glMatrix.mat4.lookAt(viewMatrix, [0, 6, -9], [0, 0, 0], [0, 1, 0]);
   gl.uniformMatrix4fv(program.phonguViewT, false, viewMatrix);
 }
 
@@ -119,31 +123,62 @@ function doLoad(theTexture, theImage) {
 // set up as well.
 //
 function setUpTextures() {
-  newTexture = gl.createTexture();
+  stemTexture = gl.createTexture();
   // gl.bindTexture(gl.TEXTURE_2D, newTexture);
 
   // loading the personalized image 1_earth_16k.jpg
   var im_age = new Image();
-  im_age.src = "img.jpg";
+  im_age.src = "darkGray.jpg";
 
   im_age.crossOrigin = "";
   im_age.onload = () => {
-    doLoad(newTexture, im_age);
+    doLoad(stemTexture, im_age);
   };
 
-  console.log("Initialized Textures");
+  tableTexture = gl.createTexture();
+  gl.bindTexture(gl.TEXTURE_2D, tableTexture);
+    
+    // loading the personalized image
+    var newImage = new Image();
+    newImage.src = 'darkWood.jpg';
+
+    newImage.onload = () => {
+        doLoad(tableTexture, newImage)
+    };
+
+    sphereTexture = gl.createTexture();
+    gl.bindTexture(gl.TEXTURE_2D, sphereTexture);
+      
+      // loading the personalized image
+      var newnewImg = new Image();
+      newnewImg.src = 'baseball.jpg';
+  
+      newnewImg.onload = () => {
+          doLoad(sphereTexture, newnewImg)
+      };
+
+    coneTexture = gl.createTexture();
+    gl.bindTexture(gl.TEXTURE_2D, coneTexture);
+      
+      // loading the personalized image
+      var ig = new Image();
+      ig.src = 'offWhite.jpg';
+  
+      ig.onload = () => {
+          doLoad(coneTexture, ig)
+      };
 }
 
-function setUpPhong(program, x, y, z) {
+function setUpPhong(program, x, y, z, x1, y1, z1, x2, y2, z2, x3, y3, z3) {
   gl.useProgram(program);
   var aL = gl.getUniformLocation(program, "ambientLight");
-  gl.uniform3fv(aL, [x, 0.5, y]);
+  gl.uniform3fv(aL, [x, y, z]);
   var lC = gl.getUniformLocation(program, "lightColor");
-  gl.uniform3fv(lC, [1.0, y, z]);
+  gl.uniform3fv(lC, [x1, y1, z1]);
   var lP = gl.getUniformLocation(program, "lightPosition");
-  gl.uniform3fv(lP, [10, 10, 4]);
+  gl.uniform3fv(lP, [x2, y2, z2]);
   var bC = gl.getUniformLocation(program, "baseColor");
-  gl.uniform3fv(bC, [x, z, 1.5]);
+  gl.uniform3fv(bC, [x3, y3, z3]);
   var specHighlightColor = gl.getUniformLocation(program, "specHighlightColor");
   gl.uniform3fv(specHighlightColor, [1.5, 1.5, 1.5]);
   var ka = gl.getUniformLocation(program, "ka");
@@ -156,65 +191,141 @@ function setUpPhong(program, x, y, z) {
   gl.uniform1f(ke, 1);
 }
 
-//
 //  This function draws all of the shapes required for your scene
-//
 function drawShapes(program) {
   gl.useProgram(program);
-
-
   gl.activeTexture(gl.TEXTURE0);
-  gl.bindTexture(gl.TEXTURE_2D, newTexture);
+  gl.bindTexture(gl.TEXTURE_2D, stemTexture);
   
+  drawTable();
+  drawStem0();
+  drawStem1();
+  drawStem2();
+  drawStem3();
+  drawCone();
+  drawBall();
+  drawBase();
 
-  // let cubeMatrix = glMatrix.mat4.create();
-  // glMatrix.mat4.scale(cubeMatrix, cubeMatrix, [1, 1, 1]);
-  // glMatrix.mat4.translate(cubeMatrix, cubeMatrix, [0, 0, 0]);
-  // gl.uniformMatrix4fv(program.uModelT, false, cubeMatrix);
-  // gl.bindVertexArray(myCube.VAO);
-  // gl.drawElements(gl.TRIANGLES, myCube.indices.length, gl.UNSIGNED_SHORT, 0);
+  function drawBall() {
+    let sphereMatrix = glMatrix.mat4.create();
+    glMatrix.mat4.scale(sphereMatrix, sphereMatrix, [0.3, 0.3, 0.3]);
+    glMatrix.mat4.translate(sphereMatrix, sphereMatrix, [0.7, 2, 0]);
+    gl.uniform1i(program.uTextureValue, 1);
+    gl.uniform3fv(program.uTheta, new Float32Array(angles));
+    gl.activeTexture(gl.TEXTURE0 + 1);
+    gl.bindTexture(gl.TEXTURE_2D, sphereTexture);
+    gl.uniform1i(program.uAbstractTexture, 1);
+    gl.uniformMatrix4fv(program.uModelT, false, sphereMatrix);
+    gl.bindVertexArray(mySphere.VAO);
+    gl.drawElements(gl.TRIANGLES, mySphere.indices.length, gl.UNSIGNED_SHORT, 0);
+  }
 
-  let cylinderMatrix = glMatrix.mat4.create();
-  glMatrix.mat4.scale(cylinderMatrix, cylinderMatrix, [0.5, 0.5, 0.5]);
-  // glMatrix.mat4.translate(cylinderMatrix, cylinderMatrix, [10, 10, 10]);
-  // gl.uniformMatrix4fv(program.aVertexPosition, false, cylinderMatrix);
-  gl.uniform1i(program.uTextureValue,1);
+  function drawBase() {
+    let coneMatrix0 = glMatrix.mat4.create();
+    glMatrix.mat4.translate(coneMatrix0, coneMatrix0, [-0.62, 0.73, 0]);
+    glMatrix.mat4.rotateZ(coneMatrix0, coneMatrix0, radians(180));
+    glMatrix.mat4.scale(coneMatrix0, coneMatrix0, [0.4, 0.035, 0.4]);
+    gl.uniform1i(program.uTextureValue, 1);
+    gl.uniform3fv(program.uTheta, new Float32Array(angles));
+    gl.activeTexture(gl.TEXTURE0 + 1);
+    gl.bindTexture(gl.TEXTURE_2D, stemTexture);
+    gl.uniform1i(program.uAbstractTexture, 1);
+    gl.uniformMatrix4fv(program.uModelT, false, coneMatrix0);
+    gl.bindVertexArray(myCone.VAO);
+    gl.drawElements(gl.TRIANGLES, myCone.indices.length, gl.UNSIGNED_SHORT, 0);
+  }
+
+  function drawCone() {
+    let coneMatrix = glMatrix.mat4.create();
+    glMatrix.mat4.translate(coneMatrix, coneMatrix, [-0.33, 0.1, 0]);
+    glMatrix.mat4.rotateZ(coneMatrix, coneMatrix, radians(100));
+    glMatrix.mat4.scale(coneMatrix, coneMatrix, [0.5, 0.35, 0.5]);
+    gl.uniform1i(program.uTextureValue, 1);
+    gl.uniform3fv(program.uTheta, new Float32Array(angles));
+    gl.activeTexture(gl.TEXTURE0 + 1);
+    gl.bindTexture(gl.TEXTURE_2D, coneTexture);
+    gl.uniform1i(program.uAbstractTexture, 1);
+    gl.uniformMatrix4fv(program.uModelT, false, coneMatrix);
+    gl.bindVertexArray(myCone.VAO);
+    gl.drawElements(gl.TRIANGLES, myCone.indices.length, gl.UNSIGNED_SHORT, 0);
+  }
+
+  function drawStem3() {
+    let cylinderMatrix3 = glMatrix.mat4.create();
+    glMatrix.mat4.translate(cylinderMatrix3, cylinderMatrix3, [-0.54, 0.40, 0]);
+    glMatrix.mat4.rotateZ(cylinderMatrix3, cylinderMatrix3, radians(-35));
+    glMatrix.mat4.scale(cylinderMatrix3, cylinderMatrix3, [0.05, 0.4, 0.2]);
+    gl.uniform1i(program.uTextureValue, 1);
+    gl.uniform3fv(program.uTheta, new Float32Array(angles));
+    gl.activeTexture(gl.TEXTURE0 + 1);
+    gl.bindTexture(gl.TEXTURE_2D, stemTexture);
+    gl.uniform1i(program.uAbstractTexture, 1);
+    gl.uniformMatrix4fv(program.uModelT, false, cylinderMatrix3);
+    gl.bindVertexArray(myCylinder.VAO);
+    gl.drawElements(gl.TRIANGLES, myCylinder.indices.length, gl.UNSIGNED_SHORT, 0);
+  }
+
+  function drawStem2() {
+    let cylinderMatrix1 = glMatrix.mat4.create();
+    glMatrix.mat4.translate(cylinderMatrix1, cylinderMatrix1, [-0.54, 0.62, 0]);
+    glMatrix.mat4.rotateZ(cylinderMatrix1, cylinderMatrix1, radians(50));
+    glMatrix.mat4.scale(cylinderMatrix1, cylinderMatrix1, [0.05, 0.2, 0.2]);
+    gl.uniform1i(program.uTextureValue, 1);
+    gl.uniform3fv(program.uTheta, new Float32Array(angles));
+    gl.activeTexture(gl.TEXTURE0 + 1);
+    gl.bindTexture(gl.TEXTURE_2D, stemTexture);
+    gl.uniform1i(program.uAbstractTexture, 1);
+    gl.uniformMatrix4fv(program.uModelT, false, cylinderMatrix1);
+    gl.bindVertexArray(myCylinder.VAO);
+    gl.drawElements(gl.TRIANGLES, myCylinder.indices.length, gl.UNSIGNED_SHORT, 0);
+  }
   
-  gl.uniform3fv (program.uTheta, new Float32Array(angles));
-  gl.activeTexture (gl.TEXTURE0+1);
-  gl.bindTexture (gl.TEXTURE_2D, newTexture);
-  gl.uniform1i (program.uAbstractTexture, 1);
-  gl.uniformMatrix4fv(program.uModelT, false, cylinderMatrix);
-  gl.bindVertexArray(myCylinder.VAO);
-  gl.drawElements(gl.TRIANGLES, myCylinder.indices.length, gl.UNSIGNED_SHORT, 0);
-  console.log("cylinder: ", myCylinder);
+  function drawTable() {
+    let cubeMatrix = glMatrix.mat4.create();
+    glMatrix.mat4.scale(cubeMatrix, cubeMatrix, [1.8, 0.1, 1]);
+    glMatrix.mat4.translate(cubeMatrix, cubeMatrix, [0, 8, 0]);
+    gl.uniform1i(program.uTextureValue, 1);
+    gl.uniform3fv(program.uTheta, new Float32Array(angles));
+    gl.activeTexture(gl.TEXTURE0 + 1);
+    gl.bindTexture(gl.TEXTURE_2D, tableTexture);
+    gl.uniform1i(program.uAbstractTexture, 1);
+    gl.uniformMatrix4fv(program.uModelT, false, cubeMatrix);
+    gl.bindVertexArray(myCube.VAO);
+    gl.drawElements(gl.TRIANGLES, myCube.indices.length, gl.UNSIGNED_SHORT, 0);
+  }
 
-  
+  function drawStem0() {
+    let cylinderMatrix0 = glMatrix.mat4.create();
+    glMatrix.mat4.translate(cylinderMatrix0, cylinderMatrix0, [-0.7, 0.55, 0]);
+    glMatrix.mat4.rotateZ(cylinderMatrix0, cylinderMatrix0, radians(-30));
+    glMatrix.mat4.scale(cylinderMatrix0, cylinderMatrix0, [0.05, 0.35, 0.2]);
+    gl.uniform1i(program.uTextureValue, 1);
+    gl.uniform3fv(program.uTheta, new Float32Array(angles));
+    gl.activeTexture(gl.TEXTURE0 + 1);
+    gl.bindTexture(gl.TEXTURE_2D, stemTexture);
+    gl.uniform1i(program.uAbstractTexture, 1);
+    gl.uniformMatrix4fv(program.uModelT, false, cylinderMatrix0);
+    gl.bindVertexArray(myCylinder.VAO);
+    gl.drawElements(gl.TRIANGLES, myCylinder.indices.length, gl.UNSIGNED_SHORT, 0);
+  }
 
-
+  function drawStem1() {
+    let cylinderMatrix = glMatrix.mat4.create();
+    glMatrix.mat4.translate(cylinderMatrix, cylinderMatrix, [-0.63, 0.25, 0]);
+    glMatrix.mat4.rotateZ(cylinderMatrix, cylinderMatrix, radians(40));
+    glMatrix.mat4.scale(cylinderMatrix, cylinderMatrix, [0.05, 0.5, 0.2]);
+    gl.uniform1i(program.uTextureValue, 1);
+    gl.uniform3fv(program.uTheta, new Float32Array(angles));
+    gl.activeTexture(gl.TEXTURE0 + 1);
+    gl.bindTexture(gl.TEXTURE_2D, stemTexture);
+    gl.uniform1i(program.uAbstractTexture, 1);
+    gl.uniformMatrix4fv(program.uModelT, false, cylinderMatrix);
+    gl.bindVertexArray(myCylinder.VAO);
+    gl.drawElements(gl.TRIANGLES, myCylinder.indices.length, gl.UNSIGNED_SHORT, 0);
+  }
 }
 
-//
-// Use this function to create all the programs that you need
-// You can make use of the auxillary function initProgram
-// which takes the name of a vertex shader and fragment shader
-//
-// Note that after successfully obtaining a program using the initProgram
-// function, you will beed to assign locations of attribute and unifirm variable
-// based on the in variables to the shaders.   This will vary from program
-// to program.
-//
-// function initPrograms() {
-
-// }
-
-//Important variables
-// perVertexProgram = initProgram('phong-per-vertex-V', 'phong-per-vertex-F');
-// ('sphereMap-V', 'sphereMap-F');
-
 function initPrograms() {
-  // const phongVertexShader = getShader("phong-per-vertex-V");
-  // const phongFragmentShader = getShader("phong-per-vertex-F");
   const textureVertexShader = getShader("sphereMap-V");
   const textureFragmentShader = getShader("sphereMap-F");
 
@@ -231,67 +342,13 @@ function initPrograms() {
 
   // Use this program instance
   gl.useProgram(program);
-  // {
-  // // We attach the location of these shader values to the program instance
-  // // for easy access later in the code
-  // // program.aVertexPosition = gl.getAttribLocation(program, "aVertexPosition");
+
   program.uModelT = gl.getUniformLocation(program, "modelt");
-  // // program.uViewT = gl.getUniformLocation(program, "viewT");
-  // // program.uProjT = gl.getUniformLocation(program, "projT");
-
-  // // program.phongAVertexPosition = gl.getAttribLocation(
-  // //   program,
-  // //   "phongAVertexPosition"
-  // // );
-  // // program.sphereAVertexPosition = gl.getAttribLocation(
-  // //   program,
-  // //   "sphereAVertexPosition"
-  // // );
-  // // program.phonguModelT = gl.getUniformLocation(program, "phongmodelT");
-  // // program.phonguViewT = gl.getUniformLocation(program, "phongviewT");
-  // // program.phonguProjT = gl.getUniformLocation(program, "phongprojT");
-
-  // // program.sphereuModelT = gl.getUniformLocation(program, "spheremodelT");
-  // // program.sphereViewT = gl.getUniformLocation(program, "sphereviewT");
-  // // program.sphereProjT = gl.getUniformLocation(program, "sphereprojT");
-
-  // //Shading Unique Uniforms
-  // // program.aNormal = gl.getAttribLocation(program, "aNormal");
-
-  // // uniforms
-  // // program.uModelT = gl.getUniformLocation(program, "modelT");
-  // // program.uViewT = gl.getUniformLocation(program, "viewT");
-  // // program.uProjT = gl.getUniformLocation(program, "projT");
-  // // program.ambientLight = gl.getUniformLocation(program, "ambientLight");
-  // // program.lightPosition = gl.getUniformLocation(program, "lightPosition");
-  // // program.lightColor = gl.getUniformLocation(program, "lightColor");
-  // // program.baseColor = gl.getUniformLocation(program, "baseColor");
-  // // program.specHighlightColor = gl.getUniformLocation(
-  // //   program,
-  // //   "specHighlightColor"
-  // // );
-  // // program.ka = gl.getUniformLocation(program, "ka");
-  // // program.kd = gl.getUniformLocation(program, "kd");
-  // // program.ks = gl.getUniformLocation(program, "ks");
-  // // program.ke = gl.getUniformLocation(program, "ke");
-
-  // //Texture Unique Uniforms
-  // // program.aUV = gl.getAttribLocation(program, "aUV");
-
-  // // uniforms - you will need to add references for any additional
-  // // uniforms that you add to your shaders
-  // // program.uTheTexture = gl.getUniformLocation(program, "theTexture");
-  // // program.uCreatedTexture = gl.getUniformLocation(program, "createdTexture");
-  // // program.uTxValue = gl.getUniformLocation(program, "txValue");
-  // // program.uTheta = gl.getUniformLocation(program, 'theta');
-  // }
 
 
   program.aVertexPosition = gl.getAttribLocation(program, 'aVertexPosition');
   program.aUV = gl.getAttribLocation(program, 'aUV');
 
-  // uniforms - you will need to add references for any additional
-  // uniforms that you add to your shaders
   program.uTheTexture = gl.getUniformLocation (program, 'theTexture');
   program.uAbstractTexture = gl.getUniformLocation(program,'abstractTexture');
   program.uTextureValue=gl.getUniformLocation(program,'textureValue');
@@ -328,98 +385,6 @@ function bindVAO(shape, program) {
     return VAO;
 }
 
-//Texture VAO
-function bindTextureVAO(shape, program) {
-  //create and bind VAO
-  let theVAO = gl.createVertexArray();
-  gl.bindVertexArray(theVAO);
-
-  // create, bind, and fill buffer for vertex locations
-  // vertex locations can be obtained from the points member of the
-  // shape object.  3 floating point values (x,y,z) per vertex are
-  // stored in this array.
-  let myVertexBuffer = gl.createBuffer();
-  gl.bindBuffer(gl.ARRAY_BUFFER, myVertexBuffer);
-  gl.bufferData(
-    gl.ARRAY_BUFFER,
-    new Float32Array(shape.points),
-    gl.STATIC_DRAW
-  );
-  gl.enableVertexAttribArray(program.sphereAVertexPosition);
-  gl.vertexAttribPointer(
-    program.sphereAVertexPosition,
-    3,
-    gl.FLOAT,
-    false,
-    0,
-    0
-  );
-
-  // create, bind, and fill buffer for uv's
-  // uvs can be obtained from the uv member of the
-  // shape object.  2 floating point values (u,v) per vertex are
-  // stored in this array.
-  let uvBuffer = gl.createBuffer();
-  gl.bindBuffer(gl.ARRAY_BUFFER, uvBuffer);
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(shape.uv), gl.STATIC_DRAW);
-  gl.enableVertexAttribArray(program.aUV);
-  gl.vertexAttribPointer(program.aUV, 2, gl.FLOAT, false, 0, 0);
-
-  // Setting up element array
-  // element indicies can be obtained from the indicies member of the
-  // shape object.  3 values per triangle are stored in this
-  // array.
-  let myIndexBuffer = gl.createBuffer();
-  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, myIndexBuffer);
-  gl.bufferData(
-    gl.ELEMENT_ARRAY_BUFFER,
-    new Uint16Array(shape.indices),
-    gl.STATIC_DRAW
-  );
-
-  // Do cleanup
-  gl.bindVertexArray(null);
-  gl.bindBuffer(gl.ARRAY_BUFFER, null);
-  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
-
-  return theVAO;
-}
-
-// // creates a VAO and returns its ID
-// function bindVAO(shape, program) {
-//   //create and bind VAO
-//   let theVAO = gl.createVertexArray();
-//   gl.bindVertexArray(theVAO);
-
-//   // create and bind vertex buffer
-//   let myVertexBuffer = gl.createBuffer();
-//   gl.bindBuffer(gl.ARRAY_BUFFER, myVertexBuffer);
-//   gl.bufferData(
-//     gl.ARRAY_BUFFER,
-//     new Float32Array(shape.points),
-//     gl.STATIC_DRAW
-//   );
-//   gl.enableVertexAttribArray(program.aVertexPosition);
-//   gl.vertexAttribPointer(program.aVertexPosition, 3, gl.FLOAT, false, 0, 0);
-
-//   // add code for any additional vertex attribute
-
-//   // Setting up the IBO
-//   let myIndexBuffer = gl.createBuffer();
-//   gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, myIndexBuffer);
-//   gl.bufferData(
-//     gl.ELEMENT_ARRAY_BUFFER,
-//     new Uint16Array(shape.indices),
-//     gl.STATIC_DRAW
-//   );
-
-//   // Clean
-//   gl.bindVertexArray(null);
-//   gl.bindBuffer(gl.ARRAY_BUFFER, null);
-//   gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
-
-//   return theVAO;
-// }
 
 /////////////////////////////////////////////////////////////////////////////
 //
@@ -540,19 +505,14 @@ function init() {
   gl.clearDepth(1.0);
   gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true); //Only for texture
 
-  // deal with keypress
   window.addEventListener('keydown', gotKey ,false);
-  // Read, compile, and link your shaders
+
   globalProgram = initPrograms();
 
-  // create and bind your current object
   createShapes();
 
-  // set up your textures
   setUpTextures();
 
+  setUpCamera(globalProgram);
 
-  // setUpCamera(globalProgram);
-  // do a draw
-  // draw();
 }
