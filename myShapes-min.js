4c1b73194a36eb475d66db3e0e47c47f8e3fb710
Karan Ahluwalia <karan9123@gmail.com>
2023-07-08 15:13:45 +0530

    Update myShapes-min.js

--- myShapes-min.js
+++ myShapes-min.js
@@ -1,18 +1,74 @@
 class Cube extends cgIShape {
     constructor(a) {
-        super(), this.makeCube(a)
+        super(); // Call the constructor of the parent class
+        this.makeCube(a); // Call the makeCube function to create the cube
     }
+
     makeCube(a) {
-        a < 1 && (a = 1);
-        var d = -.5,
-            s = -.5,
-            t = 1 / a,
-            i = 0;
+        // Ensure minimum size of 1 for the cube
+        if (a < 1) {
+            a = 1;
+        }
+
+        // Define initial positions and step size
+        var d = -0.5;
+        var s = -0.5;
+        var t = 1 / a;
+        var i = 0;
+
         for (i = 0; i < a; i++) {
             var h, r = i * t;
+
             for (h = 0; h < a; h++) {
                 var n = h * t;
-                this.addTriangle(d + n, s + r, .5, d + n + t, s + r, .5, d + n + t, s + r + t, .5), this.addNormal(0, 0, 1, 0, 0, 1, 0, 0, 1), this.adduv(n, r, n + t, r, n + t, r + t), this.addTriangle(d + n + t, s + r + t, .5, d + n, s + r + t, .5, d + n, s + r, .5), this.addNormal(0, 0, 1, 0, 0, 1, 0, 0, 1), this.adduv(n + t, r + t, n, r + t, n, r), this.addTriangle(d + n, s + r, -.5, d + n + t, s + r + t, -.5, d + n + t, s + r, -.5), this.addNormal(0, 0, -1, 0, 0, -1, 0, 0, -1), this.adduv(n, r, n + t, r + t, n + t, r), this.addTriangle(d + n, s + r, -.5, d + n, s + r + t, -.5, d + n + t, s + r + t, -.5), this.addNormal(0, 0, -1, 0, 0, -1, 0, 0, -1), this.adduv(n, r, n, r + t, n + t, r + t), this.addTriangle(-.5, s + r, d + n, -.5, s + r, d + n + t, -.5, s + r + t, d + n + t), this.addNormal(-1, 0, 0, -1, 0, 0, -1, 0, 0), this.adduv(n, r, n + t, r, n + t, r + t), this.addTriangle(-.5, s + r + t, d + n + t, -.5, s + r + t, d + n, -.5, s + r, d + n), this.addNormal(-1, 0, 0, -1, 0, 0, -1, 0, 0), this.adduv(n + t, r + t, n, r + t, n, r), this.addTriangle(.5, s + r, d + n + t, .5, s + r, d + n, .5, s + r + t, d + n), this.addNormal(1, 0, 0, 1, 0, 0, 1, 0, 0), this.adduv(n + t, r, n, r, n, r + t), this.addTriangle(.5, s + r + t, d + n, .5, s + r + t, d + n + t, .5, s + r, d + n + t), this.addNormal(1, 0, 0, 1, 0, 0, 1, 0, 0), this.adduv(n, r + t, n + t, r + t, n + t, r), this.addTriangle(d + n, .5, s + r + t, d + n + t, .5, s + r + t, d + n, .5, s + r), this.addNormal(0, 1, 0, 0, 1, 0, 0, 1, 0), this.adduv(n, r + t, n + t, r + t, n, r), this.addTriangle(d + n, .5, s + r, d + n + t, .5, s + r + t, d + n + t, .5, s + r), this.addNormal(0, 1, 0, 0, 1, 0, 0, 1, 0), this.adduv(n, r, n + t, r + t, n + t, r), this.addTriangle(d + n, -.5, s + r, d + n + t, -.5, s + r + t, d + n, -.5, s + r + t), this.addNormal(0, -1, 0, 0, -1, 0, 0, -1, 0), this.adduv(n, r, n + t, r + t, n, r + t), this.addTriangle(d + n, -.5, s + r, d + n + t, -.5, s + r, d + n + t, -.5, s + r + t), this.addNormal(0, -1, 0, 0, -1, 0, 0, -1, 0), this.adduv(n, r, n + t, r, n + t, r + t)
+
+                // Front face
+                this.addTriangle(d + n, s + r, 0.5, d + n + t, s + r, 0.5, d + n + t, s + r + t, 0.5);
+                this.addNormal(0, 0, 1, 0, 0, 1, 0, 0, 1);
+                this.addUV(n, r, n + t, r, n + t, r + t);
+
+                // Back face
+                this.addTriangle(d + n + t, s + r + t, 0.5, d + n, s + r + t, 0.5, d + n, s + r, 0.5);
+                this.addNormal(0, 0, 1, 0, 0, 1, 0, 0, 1);
+                this.addUV(n + t, r + t, n, r + t, n, r);
+
+                // Top face
+                this.addTriangle(d + n, s + r, -0.5, d + n + t, s + r + t, -0.5, d + n + t, s + r, -0.5);
+                this.addNormal(0, 0, -1, 0, 0, -1, 0, 0, -1);
+                this.addUV(n, r, n + t, r + t, n + t, r);
+
+                this.addTriangle(d + n, s + r, -0.5, d + n, s + r + t, -0.5, d + n + t, s + r + t, -0.5);
+                this.addNormal(0, 0, -1, 0, 0, -1, 0, 0, -1);
+                this.addUV(n, r, n, r + t, n + t, r + t);
+
+                // Left face
+                this.addTriangle(-0.5, s + r, d + n, -0.5, s + r, d + n + t, -0.5, s + r + t, d + n + t);
+                this.addNormal(-1, 0, 0, -1, 0, 0, -1, 0, 0);
+                this.addUV(n, r, n + t, r, n + t, r + t);
+
+                this.addTriangle(-0.5, s + r + t, d + n + t, -0.5, s + r + t, d + n, -0.5, s + r, d + n);
+                this.addNormal(-1, 0, 0, -1, 0, 0, -1, 0, 0);
+                this.addUV(n + t, r + t, n, r + t, n, r);
+
+                // Right face
+                this.addTriangle(0.5, s + r, d + n + t, 0.5, s + r, d + n, 0.5, s + r + t, d + n);
+                this.addNormal(1, 0, 0, 1, 0, 0, 1, 0, 0);
+                this.addUV(n + t, r, n, r, n, r + t);
+
+                this.addTriangle(0.5, s + r + t, d + n, 0.5, s + r + t, d + n + t, 0.5, s + r, d + n + t);
+                this.addNormal(1, 0, 0, 1, 0, 0, 1, 0, 0);
+                this.addUV(n, r + t, n + t, r + t, n + t, r);
+
+                // Bottom face
+                this.addTriangle(d + n, -0.5, s + r + t, d + n + t, -0.5, s + r + t, d + n, -0.5, s + r);
+                this.addNormal(0, 1, 0, 0, 1, 0, 0, 1, 0);
+                this.addUV(n, r + t, n + t, r + t, n, r);
+
+                this.addTriangle(d + n, -0.5, s + r, d + n + t, -0.5, s + r + t, d + n + t, -0.5, s + r);
+                this.addNormal(0, 1, 0, 0, 1, 0, 0, 1, 0);
+                this.addUV(n, r, n + t, r + t, n + t, r);
+
+                // End of loop iteration
             }
         }
     }
